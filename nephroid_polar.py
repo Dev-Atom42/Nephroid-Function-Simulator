c9a03b2c186697a5e69225c6df68878a41df723f
Atom42 <44317406+Dev-Atom42@users.noreply.github.com>
2023-07-10 21:37:29 +1000

    Add files via upload
    without fixes(

--- nephroid_polar.py
+++ nephroid_polar.py
@@ -8,7 +8,8 @@ a = float(input(" a = "))/2
 P = int(input("Size of primitive = "))
 
 theta = np.pi * 2
-rads = np.arange(0, (2 * np.pi), 0.01)
+rads = np.arange(0, (theta), 0.01)
+circumference = len(rads)
 r_points = []
 
 print("Nephroid polar")
@@ -18,14 +19,12 @@ for rad in rads:
     r_points.append(r)
 
 punct = plt.Rectangle((0, 0), 1/16, 1, color="b", visible=True)
-#punct = plt.Rectangle((0, 0), samara, samara, color='blue')
 fig, ax = plt.subplots(subplot_kw={'projection': 'polar'})
 trace, = ax.plot([], [], color="r")
 ax.grid(True)
 ax.add_artist(punct)
 ax.add_artist(trace)
 
-#ax.set_rticks([1*a, 2*a, 3*a, 4*a, 4.5*a])
 ax.set_rmax(4.4*a)
 
 def init():
@@ -43,8 +42,6 @@ def primitive(i):
     punct.set_height(P/10)
     punct.set_xy([x2-(P/20), y2-(P/20)])
     return punct,
-
-circumference = len(rads)
 
 graphic(circumference)
 anim = animation.FuncAnimation(fig, primitive,
